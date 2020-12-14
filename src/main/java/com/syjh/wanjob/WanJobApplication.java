812b02fcc1cb3e9e389567335482aa12148f25e1
joyounghun <whdudgns2654@gmail.com>
2020-12-14 21:24:07 +0900

    create project 프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/syjh/wanjob/WanJobApplication.java
@@ -0,0 +1,13 @@
+package com.syjh.wanjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WanJobApplication.class, args);
+  }
+
+}
